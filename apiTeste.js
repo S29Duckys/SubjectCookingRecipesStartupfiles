71a8ee31724b994ee8b42721c60f48a75b30aa29
s29duckys <nbarasraso@edenschool.fr>
2025-07-24 20:56:19 +0200

    Add initial implementation of cooking recipes app with API integration

---
+++ apiTeste.js
@@ -0,0 +1,15 @@
+const UrlApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+
+async function FetchApi() {
+	const res = await fetch(UrlApi)
+	const data = await res.json()
+	
+	console.log(data)
+
+	let DataStrMeal = data.meals.strMeal
+	console.log(DataStrMeal)
+}
+
+FetchApi()
+
+
